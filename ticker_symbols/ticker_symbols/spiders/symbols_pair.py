bb47956edac956626e4a2128bd53b32dc4cf31b8
Jeffrey <jiesedo@gmail.com>
2022-06-27 10:14:37 +0100

    created a new branch using extract.() in the script

--- ticker_symbols/ticker_symbols/spiders/symbols_pair.py
+++ ticker_symbols/ticker_symbols/spiders/symbols_pair.py
@@ -1,43 +1,36 @@
 import scrapy
 
-import gspread
-
-gc = gspread.service_account(file="symbolstickers.json")
-
-sh = gc.open("symbols_tickers").ticker
-
+# import gspread
+# gc = gspread.service_account(file="symbolstickers.json")
+# sh = gc.open("symbols_tickers").ticker
 
 class SymbolsPairSpider(scrapy.Spider):
     name = 'symbols_pair'
     allowed_domains = ['ng.investing.com/crypto/currency-pairs']
     start_urls = ['https://ng.investing.com/crypto/currency-pairs/']
 
+
     def parse(self, response):
-        crypto_table= response.css('table[id^="crypto_currencies_"]').getall()
+
+        # showmore = response.css(' span.arial_12.showArrowafter')
+        # for s in showmore:
+        #     yield s.css('span.arial_12.showArrowafter').
+
+        crypto_table = response.css('table[id^="crypto_currencies_"] > tbody > tr')
 
         for i in crypto_table:
-            # showmore= response.css('span.arial_12.showArrowafter').get()
-
-            item= {
-            'Name':response.css('td.left.bold.elp>a::text').get(),
-
-            'Exchange': response.css('td:nth-child(3).left::text').get(),
-            
-            'Last': response.css('td[class$="last"]::text').get(),
-
-            'Open': response.css('td:nth-child(5)::text').get(),
-
-            'High': response.css('td[class$="high"]::text').get(),
-
-            'Low': response.css('td[class$="low"]::text').get(),
-
-            'Chg': response.css('td[class$="pcp"]::text').get(),
-
-            'Vol': response.css('td[class$="pcp"] + td::text').get(),
-
-            'Time': response.css('td[class$="-time"]::text').get()
+            tickers= {
+            'showmore':i.css('span.arial_12.showArrowafter').extract(),    
+            'Name': i.css('td.left.bold.elp>a::text').extract(),
+            'Exchange': i.css('td:nth-child(3).left::text').extract(),
+            'Last': i.css('td[class$="last"]::text').extract(),
+            'Open': i.css('td:nth-child(5)::text').extract(),
+            'High': i.css('td[class$="high"]::text').extract(),
+            'Low': i.css('td[class$="low"]::text').extract(),
+            'Chg': i.css('td[class$="pcp"]::text').extract(),
+            'Vol': i.css('td[class$="pcp"] + td::text').extract(),
+            'Time': i.css('td[class$="-time"]::text').extract()
             }
-
-            yield item
-
+            yield tickers
+            # sh.append(tickers)
         pass
