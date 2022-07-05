ff4053bc7b305e068354a7537c4b2d277529b57a
Jeffrey <jiesedo@gmail.com>
2022-07-05 10:01:02 +0100

    Goal: Could populate the Google spreadsheet with the Crypto data challenges: couldn't populate the excel spread within the script but, used scrapy output option

--- READ.md
+++ READ.md
@@ -1,14 +1,15 @@
 # Scraping Ticker Smybols from CoinMarketCap
 This Bot will be used to scrape ticker symbols for cryptocurrecy pairing on the https://ng.investing.com/crypto/currency-pairs,
-the ticker symbols is inteded to develop a personal cryptocurrency trading journal using Microsoft Excel.
+the ticker symbols is intended to develop a personal cryptocurrency trading journal using Google shee and Microsoft Excel.
 
 ~ The cryptocurrency tickers will be store using a google sheet 
 
 ~ Hopefully this will also be used for more trading bot development.
 
-# I will be using the Scrapy for this project
+# I will be using the Scrapy python wev scraper for this project
 - create project folder amd venv 
 - initialize folder        
 - pip install scrapy 
-- create file: scrapy genspider <script.py> <url>        
-
+- create file: scrapy genspider <script.py> <url>  
+- suing scrapy shell to locate elements <scrapy shell "url" >      
+- using gspread to export the data
--- ticker_symbols/ticker_symbols/spiders/symbols_pair.py
+++ ticker_symbols/ticker_symbols/spiders/symbols_pair.py
@@ -1,11 +1,19 @@
+# from fileinput import filename
+# from time import time
 import scrapy
+
+import pandas as pd 
+
+import datetime
 
 import gspread
 
-gc = gspread.service_account(file="symbolstickers.json")
-
-sh = gc.open("symbols_tickers").ticker
-
+gc = gspread.service_account(filename='symbolstickers.json') # connecting the bot to the gspread worksheet
+sh = gc.open("symbols_tickers") # opening workbook
+wksheet= sh.worksheet("symbols") # pointing at the actual sheet on the workbook
+wksheet.clear() # clears the sheet of any data
+wksheet.format('A1:J1', {'textFormat': {'bold': True}}) # formatting the sheet header
+wksheet.append_row(['Name', 'Exchange', 'Last', 'Open', 'High', 'Low', 'Chg', 'Vol', 'Time', 'Cur_Time']) # Naming the data columns
 
 class SymbolsPairSpider(scrapy.Spider):
     name = 'symbols_pair'
@@ -13,31 +21,46 @@ class SymbolsPairSpider(scrapy.Spider):
     start_urls = ['https://ng.investing.com/crypto/currency-pairs/']
 
     def parse(self, response):
-        crypto_table= response.css('table[id^="crypto_currencies_"]').getall()
+        crypto_table= response.css('table[id^="crypto_currencies_"] > tbody > tr')
 
         for i in crypto_table:
-            # showmore= response.css('span.arial_12.showArrowafter').get()
+            # showmore= i.css('span.arial_12.showArrowafter').get()
 
-            item= {
-            'Name':response.css('td.left.bold.elp>a::text').get(),
-
-            'Exchange': response.css('td:nth-child(3).left::text').get(),
-            
-            'Last': response.css('td[class$="last"]::text').get(),
-
-            'Open': response.css('td:nth-child(5)::text').get(),
-
-            'High': response.css('td[class$="high"]::text').get(),
-
-            'Low': response.css('td[class$="low"]::text').get(),
-
-            'Chg': response.css('td[class$="pcp"]::text').get(),
-
-            'Vol': response.css('td[class$="pcp"] + td::text').get(),
-
-            'Time': response.css('td[class$="-time"]::text').get()
-            }
-
+            item= [{
+            'Name':i.css('td.left.bold.elp>a::text').get(),
+            'Exchange': i.css('td:nth-child(3).left::text').get(),
+            'Last': i.css('td[class$="last"]::text').get(),
+            'Open': i.css('td:nth-child(5)::text').get(),
+            'High': i.css('td[class$="high"]::text').get(),
+            'Low': i.css('td[class$="low"]::text').get(),
+            'Chg': i.css('td[class$="pcp"]::text').get(),
+            'Vol': i.css('td[class$="pcp"] + td::text').get(),
+            'Time': i.css('td[class$="-time"]::text').get(),
+            'Cur_Time': datetime.datetime.now()
+            }]
             yield item
+            
+            # appends the data to the worksheet
+            wksheet.append_row([              
+                str(item['Name']),
+                str(item['Exchange']),
+                str(item['Last']),
+                str(item['Open']),
+                str(item['High']),
+                str(item['Low']),
+                str(item['Chg']),
+                str(item['Vol']),
+                str(item['Time']),
+                str(item['Cur_Time'])
+            ])
+
+            # convert data to excel
+            sheet = pd.DataFrame(item) # converts dictionary to pandas dataframe
+            sheet.to_excel('tickers.xlsx')  # save the data ad excel
+
+            # wksheet.update([sheet.columns.values.tolist()] + sheet.values.tolist()) # updates sheet with data from dataframe
+            # print(sheet)
 
         pass
+
+
